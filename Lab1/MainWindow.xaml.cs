babbf693111deac07f9b5cdd62b7ed831e2e9aea
AbigailHerron <abigailherron@hotmail.com>
2021-01-27 11:46:18 +0000

    Update MainWindow.xaml.cs

--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -36,8 +36,15 @@ namespace Lab1
             InitializeComponent();
         }// end MainWindow
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Band b1 = new Band();
+            Band b2 = new Band("ABBA", 1972, "Agnetha Fältskog, Björn Ulvaeus, Benny Andersson, Anni-Frid Lyngstad");
+            Band b3 = new Band("Heart", 1970, "Ann Wilson, Nancy Wilson, Denny Fongheiser, Craig Bartock, Ryan Waters, Dan Walker, Andy Stoller");
+            Band b4 = new Band("Kiss", 1973, "Paul Stanley, Gene Simmons, Peter Criss, Ace Frehley");
+            Band b5 = new Band("Boppins", 2010, "Crazy Lady, Brass Tax");
+            Band b6 = new Band("In This Moment", 2005, "Maria Brink, Chris Howorth, Travis Johnson, Randy Weitzel, Kent Diimmel");
 
-
-
+        }// end Window_Loaded
     }// end MainWindow Class
 }// end Namespace
