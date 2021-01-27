70e64a1132c91a0a4b90ec0a5d26803ad21daadd
AbigailHerron <abigailherron@hotmail.com>
2021-01-27 11:05:13 +0000

    Create Band.cs

---
+++ Lab1/Band.cs
@@ -0,0 +1,24 @@
+﻿/*###########################################################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 11/01/21
+ GitHub Repository Link: https://github.com/AbigailHerron/ObjectOrientatedDevelopment/blob/main/Lab1/Lab1/Band.cs
+ 
+ Description:
+ Properties:
+ Methods:
+ Constructors: Default
+ ##########################################################################################################################*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Band
+    {
+    }
+}
