3600a2f8b5e3202ff38c66bc59aca934f09a62c9
AbigailHerron <abigailherron@hotmail.com>
2021-02-03 07:41:47 +0000

    Update MainWindow.xaml.cs

--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -43,12 +43,61 @@ namespace Lab1
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             // creating band objects
-            Band b1 = new Band();
-            Band b2 = new Band("ABBA", 1972, "Agnetha Fältskog, Björn Ulvaeus, Benny Andersson, Anni-Frid Lyngstad");
-            Band b3 = new Band("Heart", 1970, "Ann Wilson, Nancy Wilson, Denny Fongheiser, Craig Bartock, Ryan Waters, Dan Walker, Andy Stoller");
-            Band b4 = new Band("Kiss", 1973, "Paul Stanley, Gene Simmons, Peter Criss, Ace Frehley");
-            Band b5 = new Band("Boppins", 2010, "Crazy Lady, Brass Tax");
-            Band b6 = new Band("In This Moment", 2005, "Maria Brink, Chris Howorth, Travis Johnson, Randy Weitzel, Kent Diimmel");
+            PopBand b1 = new PopBand();
+            PopBand b2 = new PopBand("ABBA", 1972, "Agnetha Fältskog, Björn Ulvaeus, Benny Andersson, Anni-Frid Lyngstad");
+            RockBand b3 = new RockBand("Heart", 1970, "Ann Wilson, Nancy Wilson, Denny Fongheiser, Craig Bartock, Ryan Waters, Dan Walker, Andy Stoller");
+            RockBand b4 = new RockBand("Kiss", 1973, "Paul Stanley, Gene Simmons, Peter Criss, Ace Frehley");
+            IndieBand b5 = new IndieBand("Boppins", 2010, "Crazy Lady, Brass Tax");
+            IndieBand b6 = new IndieBand("In This Moment", 2005, "Maria Brink, Chris Howorth, Travis Johnson, Randy Weitzel, Kent Diimmel");
+
+            // creating random object
+            Random r = new Random();
+
+            // creating album objects
+            Album a1 = new Album();
+            Album a2 = new Album("Now That's What I Call Music", r.Next(1960, 2020), r.Next(1000000, 10000000));
+
+                // ABBA
+            Album a3 = new Album("Waterloo", r.Next(1960, 2020), r.Next(1000000, 10000000));
+            Album a4 = new Album("Arrival", r.Next(1960, 2020), r.Next(1000000, 10000000));
+
+                // Heart
+            Album a5 = new Album("Little Queen", r.Next(1960, 2020), r.Next(1000000, 10000000));
+            Album a6 = new Album("Brigade", r.Next(1960, 2020), r.Next(1000000, 10000000));
+
+                // Kiss
+            Album a7 = new Album("Psycho Circus", r.Next(1960, 2020), r.Next(1000000, 10000000));
+            Album a8 = new Album("Alive II", r.Next(1960, 2020), r.Next(1000000, 10000000));
+
+                // In This Moment
+            Album a9 = new Album("Black Widow", r.Next(1960, 2020), r.Next(1000000, 10000000));
+            Album a10 = new Album("Beautiful Tragedy", r.Next(1960, 2020), r.Next(1000000, 10000000));
+
+                // Boppins
+            Album a11 = new Album("I Don't Know How I Got Here", r.Next(1960, 2020), r.Next(1000000, 10000000));
+            Album a12 = new Album("Up Is Not Jump", r.Next(1960, 2020), r.Next(1000000, 10000000));
+
+
+            // adding albums to respective bands
+            b1.AlbumList.Add(a1);
+            b1.AlbumList.Add(a2);
+
+            b2.AlbumList.Add(a3);
+            b2.AlbumList.Add(a4);
+
+            b3.AlbumList.Add(a5);
+            b3.AlbumList.Add(a6);
+
+            b4.AlbumList.Add(a7);
+            b4.AlbumList.Add(a8);
+
+            b5.AlbumList.Add(a11);
+            b5.AlbumList.Add(a12);
+
+            b6.AlbumList.Add(a9);
+            b6.AlbumList.Add(a10);
+
+
 
             // adding band objects to bands list
             bands.Add(b1);
@@ -58,6 +107,8 @@ namespace Lab1
             bands.Add(b5);
             bands.Add(b6);
 
+            
+
             // sorting bands list by BandName
             bands.Sort();
 
@@ -65,5 +116,24 @@ namespace Lab1
             lbxBands.ItemsSource = bands;
 
         }// end Window_Loaded
+
+
+        /*Method: lbxBands_SelectionChanged
+                  1) Executes when an item in the lbxBands list box is selected
+                  2) Checks if something was selected or not
+                  3) Displays band information in the tblkInfoBand text block and
+                     lbxAlbums list box */
+        private void lbxBands_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Band selectedBand = lbxBands.SelectedItem as Band;
+
+            if(selectedBand != null)
+            {
+                lbxAlbums.ItemsSource = selectedBand.AlbumList;
+                tblkInfoBand.Text = string.Format($"Formed in {selectedBand.YearFormed}" +
+                                     $"/nMembers: {selectedBand.Members}");
+            }// end if block
+        }// end lbxBands_SelectionChanged()
+
     }// end MainWindow Class
 }// end Namespace
