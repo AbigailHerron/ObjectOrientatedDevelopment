6de05b1496f8836a6d1dc74124a3d85b8ac8ed00
AbigailHerron <abigailherron@hotmail.com>
2021-02-03 06:30:18 +0000

    Create Album.cs

---
+++ Lab1/Album.cs
@@ -0,0 +1,28 @@
+﻿/*###########################################################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 11/01/21
+ GitHub Repository Link: https://github.com/AbigailHerron/ObjectOrientatedDevelopment/blob/main/Lab1/Band.cs
+ 
+ Description: A blueprint for a musical Album object
+ Properties: Album Name, Released, Sales
+ Methods: 
+ Constructors: Default, All
+ ##########################################################################################################################*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Album
+    {
+        /*PROPERTIES ------------------------------------------------------------------------------------------------------*/
+        public string AlbumName { get; set; }
+        public int YearFormed { get; set; }
+        public decimal Sales { get; set; }
+    }// end Album class
+}
