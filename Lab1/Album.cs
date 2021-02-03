558d049a09fe1aadc0267fc4bbf56da0a4395d50
AbigailHerron <abigailherron@hotmail.com>
2021-02-03 07:41:28 +0000

    Update Album.cs

--- Lab1/Album.cs
+++ Lab1/Album.cs
@@ -3,11 +3,11 @@
  ID: S00200536
  Year: 2
  Date: 11/01/21
- GitHub Repository Link: https://github.com/AbigailHerron/ObjectOrientatedDevelopment/blob/main/Lab1/Band.cs
+ GitHub Repository Link: https://github.com/AbigailHerron/ObjectOrientatedDevelopment/blob/main/Lab1/Album.cs
  
  Description: A blueprint for a musical Album object
- Properties: Album Name, Released, Sales
- Methods: 
+ Properties: AlbumName, Released, Sales
+ Methods: ToString() (override)
  Constructors: Default, All
  ##########################################################################################################################*/
 using System;
@@ -18,11 +18,42 @@ using System.Threading.Tasks;
 
 namespace Lab1
 {
-    class Album
+    public class Album
     {
         /*PROPERTIES ------------------------------------------------------------------------------------------------------*/
         public string AlbumName { get; set; }
         public int YearFormed { get; set; }
-        public decimal Sales { get; set; }
+        public int Sales { get; set; }
+
+        /*CONSTRUCTORS ----------------------------------------------------------------------------------------------------*/
+        /*Constructor: Default
+                  1) Chains to All Constructor
+                  2) Passes down the default values */
+        public Album() : this("Hits of the Year", 2021, 1000)
+        {
+        }// end Default Constructor
+
+
+        /*Constructor: All
+                  1) Takes in a string value and two int values
+                  2) Creates an Album object with the AlbumName name, YearFormed year
+                     and Sales sales */
+        public Album(string name, int year, int sales)
+        {
+            this.AlbumName = name;
+            this.YearFormed = year;
+            this.Sales = sales;
+        }// end All Constructor
+
+
+
+        /*METHODS ---------------------------------------------------------------------------------------------------------*/
+        /*Method: ToString()
+                  1) Overrides original ToString() method */
+        public override string ToString()
+        {
+            return string.Format($"{this.AlbumName} - released in {this.YearFormed}. Sales of {this.Sales}");
+        }// end ToString()
+
     }// end Album class
 }
