60f68b4f43568c2e849bd7a597bf338ea1e6e41a
AbigailHerron <abigailherron@hotmail.com>
2021-02-10 15:58:13 +0000

    Create App.xaml.cs

---
+++ CardGameInterfaceProject/CardGameInterfaceProject/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CardGameInterfaceProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
