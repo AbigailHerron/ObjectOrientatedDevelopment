c571f9b58d1a8c084712c48e225ac4acd7383243
AbigailHerron <abigailherron@hotmail.com>
2021-02-11 06:56:51 +0000

    Update Deck.cs

--- CardGameInterfaceProject/CardGameInterfaceProject/Deck.cs
+++ CardGameInterfaceProject/CardGameInterfaceProject/Deck.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿/*####################################################################################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 11/02/21
+ GitHub Link: https://github.com/AbigailHerron/ObjectOrientatedDevelopment/blob/main/CardGameInterfaceProject/CardGameInterfaceProject/Deck.cs
+ 
+ Description: An abstract class (Deck) and all related Sub-Classes (PlayingDeck, TarotDeck) as follows:
+ --------------------------------------------------------------------
+     Class: Deck (abstract)
+     Properties: Deck, SpecialCards
+     Methods: Shuffle
+
+             Sub-Class: PlayingDeck
+             Additional Properties: --
+             Additional Methods: ToString(override)
+             Constructors: Default
+
+             Sub-Class: TarotDeck
+             Additional Properties: --
+             Additional Methods: ToString(override)
+             Constructors: Default
+ --------------------------------------------------------------------
+ ###################################################################################################################################################*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +30,19 @@ using System.Threading.Tasks;
 
 namespace CardGameInterfaceProject
 {
-    class Deck
+    public abstract class Deck
     {
-    }
-}
+        /*PROPERTIES ------------------------------------------------------------------------------------------------------*/
+        public List<Card> Pack { get; set; }
+        public string[] SpecialCards { get; set; }
+
+        /*METHODS ---------------------------------------------------------------------------------------------------------*/
+        /*Method: CreateDeck
+                  1) */
+
+        /*Method: Shuffle
+                  1) */
+
+
+    }// end Deck abstract class
+}// end Namespace
