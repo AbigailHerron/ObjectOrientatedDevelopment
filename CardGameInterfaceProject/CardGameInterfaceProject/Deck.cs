801a302105cceb1175fa252ea1ffdfac26db4d04
AbigailHerron <abigailherron@hotmail.com>
2021-02-11 07:22:32 +0000

    Update Deck.cs

--- CardGameInterfaceProject/CardGameInterfaceProject/Deck.cs
+++ CardGameInterfaceProject/CardGameInterfaceProject/Deck.cs
@@ -7,7 +7,7 @@
  
  Description: An abstract class (Deck) and all related Sub-Classes (PlayingDeck, TarotDeck) as follows:
  --------------------------------------------------------------------
-     Class: Deck (abstract)
+     Class: Deck (abstract) -- Note: May change this to a Parent class later
      Properties: Deck, SpecialCards
      Methods: Shuffle
 
@@ -37,11 +37,29 @@ namespace CardGameInterfaceProject
         public string[] SpecialCards { get; set; }
 
         /*METHODS ---------------------------------------------------------------------------------------------------------*/
-        /*Method: CreateDeck
-                  1) */
 
         /*Method: Shuffle
-                  1) */
+                  1) To be executed after CreateDeck has run
+                  2) Re-orders the card objects in the list Pack */
+        public void Shuffle()
+        {
+            Random order = new Random();
+            Card temp = new Card();
+            int cardIndex;
+
+            for (int i = 0; i < this.Pack.Count; i++)
+            {
+                // makes cardIndex a random number within the limited range of cards in the list Pack
+                cardIndex = order.Next(this.Pack.Count);
+
+                    // keeping the details of card object at i in the list
+                temp = this.Pack.ElementAt(i);
+                    // makes the card at i now equal a random card in the Pack list
+                this.Pack[i] = this.Pack.ElementAt(cardIndex); 
+                    // puts original card at i (temp) where the index of the random card is
+                this.Pack[cardIndex] = temp; // dealing with duplicates
+            }// end for block
+        }// end Shuffle()
 
 
     }// end Deck abstract class
