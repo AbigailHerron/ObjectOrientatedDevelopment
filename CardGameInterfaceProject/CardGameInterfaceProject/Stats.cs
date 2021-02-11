0ffbbf137304155e67881db2f08d53ade1156035
AbigailHerron <abigailherron@hotmail.com>
2021-02-11 05:19:10 +0000

    Create Stats.cs

---
+++ CardGameInterfaceProject/CardGameInterfaceProject/Stats.cs
@@ -0,0 +1,24 @@
+﻿/*##########################################################################################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 11/02/21
+ GitHub Link: https://github.com/AbigailHerron/ObjectOrientatedDevelopment/blob/main/CardGameInterfaceProject/CardGameInterfaceProject/Player.cs
+ 
+ Description: A blueprint for a Player's Statistics Record object
+ Properties: Wins, Losses, Draws
+ Methods:
+ Constructors: Default
+ #########################################################################################################################################################*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameInterfaceProject
+{
+    public class Stats
+    {
+    }// end Stats class
+}// end Namespace
