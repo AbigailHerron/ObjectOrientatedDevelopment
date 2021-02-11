4f7ba8f5c9d9d697ae638d08629d9c7f2154a1a2
AbigailHerron <abigailherron@hotmail.com>
2021-02-11 05:06:37 +0000

    Create Player.cs

---
+++ CardGameInterfaceProject/CardGameInterfaceProject/Player.cs
@@ -0,0 +1,24 @@
+﻿/*###########################################################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 11/02/21
+ GitHub Repository Link: 
+ 
+ Description:
+ Properties:
+ Methods:
+ Constructors: Default
+ ##########################################################################################################################*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameInterfaceProject
+{
+    class Player
+    {
+    }// end Player class
+}// end Namespace
