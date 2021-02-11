7b3dbb5cb92561241e989a72cb24e86dcf96f1c1
AbigailHerron <abigailherron@hotmail.com>
2021-02-11 05:19:07 +0000

    Update Player.cs

--- CardGameInterfaceProject/CardGameInterfaceProject/Player.cs
+++ CardGameInterfaceProject/CardGameInterfaceProject/Player.cs
@@ -1,15 +1,15 @@
-﻿/*###########################################################################################################################
+﻿/*##########################################################################################################################################################
  Name: Abigail Herron
  ID: S00200536
  Year: 2
  Date: 11/02/21
- GitHub Repository Link: 
+ GitHub Link: https://github.com/AbigailHerron/ObjectOrientatedDevelopment/blob/main/CardGameInterfaceProject/CardGameInterfaceProject/Player.cs
  
- Description:
- Properties:
+ Description: A blueprint for a Player object
+ Properties: PlayerName, Stats
  Methods:
- Constructors: Default
- ##########################################################################################################################*/
+ Constructors: Default, All
+ #########################################################################################################################################################*/
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +18,23 @@ using System.Threading.Tasks;
 
 namespace CardGameInterfaceProject
 {
-    class Player
+    public class Player
     {
+        /*PROPERTIES ------------------------------------------------------------------------------------------------------*/
+        public string PlayerName { get; set; }
+        public List<Stats> Scores {get; set;}
+
+
+        /*CONSTRUCTORS ----------------------------------------------------------------------------------------------------*/
+        /*Constructor: Default
+                       1) Initialises a blank Player with default values
+                       2) Passes default values to All constructor */
+        public Player() : this("Guest") { }
+
+        public Player(string name)
+        {
+            this.PlayerName = name;
+                this.Scores = new List<Stats>();
+        }
     }// end Player class
 }// end Namespace
