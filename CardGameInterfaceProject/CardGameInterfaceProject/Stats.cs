499ea058035c78c6a4c05e5bff643775ba39e1a0
AbigailHerron <abigailherron@hotmail.com>
2021-02-11 05:52:42 +0000

    Update Stats.cs

--- CardGameInterfaceProject/CardGameInterfaceProject/Stats.cs
+++ CardGameInterfaceProject/CardGameInterfaceProject/Stats.cs
@@ -6,8 +6,8 @@
  GitHub Link: https://github.com/AbigailHerron/ObjectOrientatedDevelopment/blob/main/CardGameInterfaceProject/CardGameInterfaceProject/Stats.cs
  
  Description: A blueprint for a Player's Statistics Record object
- Properties: Wins, Losses, Draws
- Methods:
+ Properties: GameCode, Wins, Losses, Draws
+ Methods: UpdateStats
  Constructors: Default
  #########################################################################################################################################################*/
 using System;
@@ -21,8 +21,38 @@ namespace CardGameInterfaceProject
     public class Stats
     {
         /*PROPERTIES ------------------------------------------------------------------------------------------------------*/
+        public string GameCode { get; set; }
         public int Wins { get; set; }
         public int Losses { get; set; }
         public int Draws { get; set; }
+
+        /*CONSTRUCTORS ----------------------------------------------------------------------------------------------------*/
+        /*Constructor: Default
+                       1) Initialises a blank Stats object
+                       2) Passes a default value of 0 to all values */
+        public Stats()
+        {
+            this.GameCode = "N/A";
+            this.Wins = 0;
+            this.Losses = 0;
+            this.Draws = 0;
+        }// end Default constructor
+
+        /*METHODS ---------------------------------------------------------------------------------------------------------*/
+        /*Method: UpdateStats()
+                  1) Takes in an int value (expected range 1-3)
+                  2) Divides this number by 3
+                  3) Updates the stats based on the remainder */
+        public void UpdateStats(int result)
+        {
+            int data = result % 3;
+
+            if (data == 0)
+                this.Draws++;
+            else if (data == 1)
+                this.Wins++;
+            else
+                this.Losses++;
+        }// end UpdateStats()
     }// end Stats class
 }// end Namespace
