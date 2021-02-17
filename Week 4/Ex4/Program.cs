3898b8b37619399779c132032e9f8f7384e8b931
AbigailHerron <abigailherron@hotmail.com>
2021-02-17 12:24:48 +0000

    Create Program.cs

---
+++ Week 4/Ex4/Program.cs
@@ -0,0 +1,47 @@
+﻿/*###########################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 17/02/21
+
+ Description: Week 4, Exercise 4 - practicing LINQ
+              - Deferred Exectution
+ ###########################################################################*/
+using System;
+using System.Linq;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1,5,3,6,10,12,8};
+            /* // Version 1 - Query
+            var query = from number in numbers
+                        select DoubleIt(number);
+            // end v1 */
+
+            // Version 2 - Lambda
+            var query = numbers
+                .Select(n => DoubleIt(n));
+            // end v2
+
+            Console.WriteLine("Before the foreach loop");
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }// end Main()
+
+        // supporting DoubleIt method
+        private static int DoubleIt (int value)
+        {
+            Console.WriteLine("About to double the number " + value.ToString());
+            return value * 2;
+        }// end DoubleIt()
+    }// end class
+}// end namespace
